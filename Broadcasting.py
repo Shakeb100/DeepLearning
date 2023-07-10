161fcbc40900c4adb64596913da7af461847cdd2
Shakeb Siddiqui <smsicon2001@gmail.com>
2023-07-10 15:47:59 -0400

    tricks

--- Broadcasting.py
+++ Broadcasting.py
@@ -4,10 +4,23 @@ A = np.array([[56.0, 0.0, 4.4, 68.0,],
               [1.2, 104.0, 52.0, 8.0],
               [1.8, 135.0, 99.0, 0.9]])
 
-print(A)
 
 cal = A.sum(axis=0) #axis 0 = vertically, axis 1 = horizontally
-print(cal)
 
 percentage = 100* A/cal.reshape(1,4) #gives percentage of each value by column
-print(percentage) 
+ 
+
+########################tips and tricks to avoid bugs############################################
+
+#constructing vectors
+
+a = np.random.randn(5) #we don't want to do this !!!!!!!
+#rank 1 array
+
+a = np.random.randn(5,1) #do this instead
+
+print("This is a: ", a)
+print("This is the transpose: ", a.T)
+print("The product is: ", np.dot(a,a.T))
+
+
