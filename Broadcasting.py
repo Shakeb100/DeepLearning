87f3af8c67bf22e0a6b5bec07a1c1152bcbac205
Shakeb Siddiqui <smsicon2001@gmail.com>
2023-07-13 12:32:48 -0400

    testing shape

--- Broadcasting.py
+++ Broadcasting.py
@@ -10,17 +10,38 @@ cal = A.sum(axis=0) #axis 0 = vertically, axis 1 = horizontally
 percentage = 100* A/cal.reshape(1,4) #gives percentage of each value by column
  
 
-########################tips and tricks to avoid bugs############################################
+######################## tips and tricks to avoid bugs ############################################
 
 #constructing vectors
 
 a = np.random.randn(5) #we don't want to do this !!!!!!!
 #rank 1 array
 
-a = np.random.randn(5,1) #do this instead
+a = np.random.randn(5,1) #do this instead. is more explicit
 
-print("This is a: ", a)
-print("This is the transpose: ", a.T)
-print("The product is: ", np.dot(a,a.T))
+#print("This is a: ", a)
+#print("This is the transpose: ", a.T)
+#print("The product is: ", np.dot(a,a.T))
 
 
+ex = np.array([[2,1],[1,3]])
+ans = np.dot(ex,ex)
+
+
+
+a = np.random.randn(3,3)
+b = np.random.randn(3,1)
+c = a+b
+
+
+
+a = np.array([[1,1],[1,-1]])
+b = np.array([[2],[3]])
+n = a + b
+print(n)
+
+
+A = np.random.rand(4,3)
+B = np.sum(A, axis = 1, keepdims=True)
+
+print("The shape is: ", np.shape(B))
